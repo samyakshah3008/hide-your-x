f08f4d7cdd921956665675b4f783e299d8f3049e
Samyak Shah <samyakshah3008@gmail.com>
2024-06-28 15:59:20 +0530

    Feat: User DAO add

---
+++ hideyourx/src/main/java/dao/UserDAO.java
@@ -0,0 +1,35 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import db.MyConnection;
+import models.User;
+
+public class UserDAO {
+    public static boolean isExists(String email) throws SQLException {
+        Connection connection = MyConnection.getConnection();
+        PreparedStatement ps = connection.prepareStatement("select email from users");
+        ResultSet rs = ps.executeQuery();
+        // rs will keep giving us all the emails from DB, and below we are checking with
+        // the parameter's email
+        while (rs.next()) {
+            String e = rs.getString(1);
+            if (e.equals(email)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static int saveUser(User user) throws SQLException {
+        Connection connection = MyConnection.getConnection();
+        PreparedStatement ps = connection.prepareStatement("insert into users values(default , ?, ?)");
+        ps.setString(1, user.getName());
+        ps.setString(2, user.getEmail());
+        return ps.executeUpdate();
+    }
+}
