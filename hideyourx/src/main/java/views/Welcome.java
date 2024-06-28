d76a663b7c7229611a68e88d6373675448d6b11b
Samyak Shah <samyakshah3008@gmail.com>
2024-06-28 16:54:48 +0530

    Feat: end to end auth logic done

--- hideyourx/src/main/java/views/Welcome.java
+++ hideyourx/src/main/java/views/Welcome.java
@@ -3,6 +3,14 @@ package views;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import dao.UserDAO;
+import models.User;
+import services.GenerateOTP;
+import services.SendOTPService;
+import services.UserService;
 
 public class Welcome {
     public void welcomeScreen() {
@@ -28,10 +36,55 @@ public class Welcome {
     }
 
     private void login() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your registered email");
+        String email = sc.nextLine();
 
+        try {
+            if (UserDAO.isExists(email)) {
+                String genOTP = GenerateOTP.getOTP();
+                SendOTPService.sendOTP(email, genOTP);
+                System.out.println("Enter the OTP");
+
+                String otp = sc.nextLine();
+
+                if (otp.equals(genOTP)) {
+                    System.out.println("Welcome, successful login");
+                } else {
+                    System.out.println("WRONG OTP");
+                }
+            } else {
+                System.out.println("User not found");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     private void signUp() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name");
+        String name = sc.nextLine();
+        System.out.println("Enter email");
+        String email = sc.nextLine();
+        String genOTP = GenerateOTP.getOTP();
+        SendOTPService.sendOTP(email, genOTP);
+        System.out.println("Enter the otp");
+
+        String otp = sc.nextLine();
+
+        if (otp.equals(genOTP)) {
+            User user = new User(name, email);
+
+            int response = UserService.saveUser(user);
+
+            switch (response) {
+                case 0 -> System.out.println("User registered");
+                case 1 -> System.out.println("User already exists");
+            }
+        } else {
+            System.out.println("WRONG OTP");
+        }
 
     }
 }
